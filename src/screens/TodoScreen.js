02a4d6e90beb0e1d766cc689e063db5b9f0f3af1
info-sapphire <info.sapphire@yandex.ru>
2020-12-20 11:14:03 +0300

    feat: add theme

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,15 +1,32 @@
 import React from 'react'
 import { View, Text, Button, StyleSheet } from 'react-native'
+import { THEME } from '../theme'
 
 export const TodoScreen = ({ goBack, todo }) => {
   return (
     <View style={styles.wrapper}>
       <Text>{todo.title}</Text>
-      <Button title="Назад" onPress={goBack}  />
+      <View style={styles.buttons}>
+        <View style={styles.button}>
+          <Button title="Назад" color={THEME.GRAY_COLOR} onPress={goBack} />
+        </View>
+        <View style={styles.button}>
+          <Button title="Удалить" color={THEME.DANGER_COLOR}  onPress={goBack} />
+        </View>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  wrapper: {}
+  wrapper: {},
+
+  button: {
+    flex: 1
+  },
+
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
 })
---
+++ src/theme.js
@@ -0,0 +1,5 @@
+export const THEME = {
+  DANGER_COLOR: '#e53635',
+  GRAY_COLOR: '#757575',
+  MAIN_COLOR: '#3949ab'
+}
